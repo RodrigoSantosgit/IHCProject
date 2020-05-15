8edec44f47160c23e40900b925770df720b87d90
Rodrigo Santos <rodrigo.l.silva.santos@ua.pt>
2020-05-15 17:48:39 +0100

    Stats

---
+++ IHCProject/InitialPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IHCProject
+{
+    /// <summary>
+    /// Interaction logic for InitialPage.xaml
+    /// </summary>
+    public partial class InitialPage : Window
+    {
+        public InitialPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StatisticsWindow stats = new StatisticsWindow();
+            stats.Show();
+            this.Hide();
+
+        }
+    }
+}
--- IHCProject/MainWindow.xaml.cs
+++ IHCProject/MainWindow.xaml.cs
@@ -18,11 +18,12 @@ namespace IHCProject
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class StatisticsWindow : Window
     {
-        public MainWindow()
+        public StatisticsWindow()
         {
             InitializeComponent();
         }
+
     }
 }

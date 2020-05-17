08aafc3bb0707994d2c5b5f30e6a6db67927ece6
Daniel Lopes <daniel.vnl@ua.pt>
2020-05-17 19:45:50 +0100

    adicionei " room " só para testar (façam backup antes)

--- IHCProject/obj/Debug/IHCProject_Content.g.i.cs
+++ IHCProject/obj/Debug/IHCProject_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/backarrow.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("1%20-%20rooms.png")]
 
 
